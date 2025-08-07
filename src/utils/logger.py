1a20537dcfd0619fedd75f4333aa62b9b3363e9f
dev-sereti <devsereti@gmail.com>
2025-08-07 23:45:40 +0300

    logger added.

---
+++ src/utils/logger.py
@@ -0,0 +1,17 @@
+from loguru import logger
+import sys
+from src.utils.config_loader import config
+
+# Configure logger
+logger.remove()
+logger.add(
+    sys.stdout,
+    level=config.log_level,
+    format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>"
+)
+logger.add(
+    "logs/network-monitor.log",
+    rotation="10 MB",
+    retention="7 days",
+    level=config.log_level
+)
